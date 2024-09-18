397b052c2343809953d55c964339a5e5cb47f718
jin hammond <121529261+jinexists@users.noreply.github.com>
2024-09-18 12:24:09 -0500

    remove funky defaults, hello world

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,13 +5,6 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
