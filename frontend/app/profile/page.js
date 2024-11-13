28e7a463dd05e5fdf8dd59f989d1001df822fd49
Samuel Breider <samueljbreider@gmail.com>
2024-11-12 20:29:58 -0600

    import cleanup / delete game cookie logout

--- frontend/app/profile/page.js
+++ frontend/app/profile/page.js
@@ -1,7 +1,17 @@
+import prisma from "../_utils/db";
+import { authorizeUserRoute } from "../_utils/userSession";
+
 export default async function ProfileIndex() {
+  const { session } = await authorizeUserRoute();
+  const userInDB = await prisma.user.findFirst({
+    where: { email: session.email },
+  });
+
   return (
     <main>
-      <div className="p-3">Nothing yet, come back soon</div>
+      <div>
+        <p>{"Thanks for joining Gopher Guessr, " + userInDB.name + "!"}</p>
+      </div>
     </main>
   );
 }
