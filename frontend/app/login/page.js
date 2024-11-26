80d760a22b7e3c22c42599c5cdfd33fa0e3a8b8d
Divyesh Thirukonda <72324738+Divyesh-Thirukonda@users.noreply.github.com>
2024-11-25 23:46:30 -0600

    Updated Favicon, Styled login page
    Favicon has a transparent bg

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -1,5 +1,3 @@
-// login page
-
 import Script from "next/script";
 import { redirect } from "next/navigation";
 import { getUserSession } from "../_utils/userSession";
@@ -10,26 +8,30 @@ export default async function UserAuth() {
   if (session.email) redirect("/profile");
 
   return (
-    <div className="p-3">
-      <h1 className="mb-1.5 text-3xl font-bold">User Login</h1>
-      <div
-        id="g_id_onload"
-        data-client_id={process.env.NEXT_PUBLIC_GOOGLE_ADMIN}
-        data-context="signin"
-        data-ux_mode="popup"
-        data-login_uri={`${process.env.ROOT}/login/continue`}
-        data-auto_prompt="false"
-        data-use_fedcm_for_prompt="true"
-      ></div>
-      <div
-        className="g_id_signin"
-        data-type="standard"
-        data-shape="pill"
-        data-theme="outline"
-        data-text="signin_with"
-        data-size="large"
-        data-logo_alignment="left"
-      ></div>
+    <div className="flex items-center justify-center min-h-screen bg-rose-600">
+      <div className="p-6 bg-white rounded-lg shadow-lg">
+        <h1 className="mb-6 text-5xl font-extrabold text-rose-700 text-center">
+          User Login
+        </h1>
+        <div
+          id="g_id_onload"
+          data-client_id={process.env.NEXT_PUBLIC_GOOGLE_ADMIN}
+          data-context="signin"
+          data-ux_mode="popup"
+          data-login_uri={`${process.env.ROOT}/login/continue`}
+          data-auto_prompt="false"
+          data-use_fedcm_for_prompt="true"
+        ></div>
+        <div
+          className="g_id_signin mx-auto flex items-center justify-center"
+          data-type="standard"
+          data-shape="pill"
+          data-theme="outline"
+          data-text="signin_with"
+          data-size="large"
+          data-logo_alignment="left"
+        ></div>
+      </div>
       <Script src="https://accounts.google.com/gsi/client" />
     </div>
   );
