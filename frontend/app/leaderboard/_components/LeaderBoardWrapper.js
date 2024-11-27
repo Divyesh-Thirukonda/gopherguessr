2ca57c1ba850308cf9ef81fe6e298aa3d08cc4ed
Samuel Breider <samueljbreider@gmail.com>
2024-11-26 20:19:55 -0600

    adjust background + home button

--- frontend/app/leaderboard/_components/LeaderBoardWrapper.js
+++ frontend/app/leaderboard/_components/LeaderBoardWrapper.js
@@ -4,45 +4,40 @@ import { ArrowRight } from "@phosphor-icons/react/dist/ssr";
 import * as motion from "framer-motion/client";
 import Link from "next/link";
 import Table from "./Table";
+import { ArrowArcLeft, ArrowLeft } from "@phosphor-icons/react";
 
 export default function LeaderBoardWrapper({ isLoggedIn }) {
   return (
-    <main>
-      <section className="relative min-h-dvh w-full">
-        <div className="fixed inset-0">
-          <video
-            className="h-full w-full object-cover"
-            autoPlay
-            loop
-            muted
-            playsInline
-            poster="/cacheable/umn-flyover-poster-20241021.webp"
+    <main className="min-h-screen bg-gradient-to-br from-yellow-400 to-rose-800">
+      <div className="absolute inset-0 flex items-center justify-center px-4 pb-20 pt-12">
+        <div className="max-w-md text-center">
+          <Table />
+          <motion.div
+            className="absolute right-0 top-0 mx-auto mr-3 mt-4 inline-block rounded-full bg-rose-600"
+            whileHover={{ scale: 1.1 }}
           >
-            <source
-              src="/cacheable/umn-flyover-20241021.mp4"
-              type="video/mp4"
-            />
-          </video>
-        </div>
-        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 backdrop-blur"></div>
-        <div className="absolute inset-0 flex items-center justify-center px-4 pb-20 pt-12">
-          <div className="max-w-md text-center">
-            <Table />
-            <motion.div
-              className="absolute right-0 top-0 mx-auto mr-3 mt-4 inline-block rounded-full bg-rose-600"
-              whileHover={{ scale: 1.1 }}
+            <Link
+              href="/login"
+              className="inline-flex items-center px-4 py-2 text-2xl font-medium text-white"
             >
-              <Link
-                href="/login"
-                className="inline-flex items-center px-4 py-2 text-2xl font-medium text-white"
-              >
-                {isLoggedIn ? "Profile" : "Login"}
-                <ArrowRight className="ml-1.5 h-6 w-6" weight="bold" />
-              </Link>
-            </motion.div>
-          </div>
+              {isLoggedIn ? "Profile" : "Login"}
+              <ArrowRight className="ml-1.5 h-6 w-6" weight="bold" />
+            </Link>
+          </motion.div>
+          <motion.div
+            className="absolute left-0 top-0 mx-auto ml-3 mt-4 inline-block rounded-full bg-rose-600"
+            whileHover={{ scale: 1.1 }}
+          >
+            <Link
+              href="/"
+              className="inline-flex items-center px-4 py-2 text-2xl font-medium text-white"
+            >
+              Home
+              <ArrowArcLeft className="ml-1.5 h-6 w-6" weight="bold" />
+            </Link>
+          </motion.div>
         </div>
-      </section>
+      </div>
     </main>
   );
 }
