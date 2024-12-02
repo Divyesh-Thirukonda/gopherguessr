ecab90154d45fb5cca0ea344ae8c7af51b58747e
Keerthik Muruganandam <70237309+krthkmgndm@users.noreply.github.com>
2024-12-02 11:24:37 -0600

    vertical + fixed visual

--- frontend/app/play/_components/ResultsDialog.js
+++ frontend/app/play/_components/ResultsDialog.js
@@ -82,65 +82,22 @@ export default function ResultsDialog({
           />
         </div>
       )}
-      <div className="pointer-events-none absolute bottom-24 left-0 right-0 z-[1200] mx-4 bg-opacity-40 shadow-xl backdrop-blur-md">
-        <div className="relative h-6 rounded-full bg-slate-500 shadow-xl">
+      <div className="pointer-events-none absolute bottom-0 right-0 top-0 z-[1200] m-2 bg-opacity-40">
+        <div className="relative h-full w-6 overflow-hidden rounded-full bg-slate-400">
           <div
-            className="absolute left-0 top-0 z-[1300] h-6 rounded-full bg-rose-600 shadow-lg transition-[width] duration-700 ease-out"
-            style={{ width: `${progress}%` }}
+            className="absolute bottom-0 left-0 w-full rounded-full bg-rose-600 transition-[height] duration-700 ease-out"
+            style={{ height: `${progress}%` }} // Correct progress height
           >
-            {/* UNCOMMENT THE BELOW OUT IF YOU WANT MORE DETAIL */}
-            {/* {(curState.points > 50 && curState.points < 500) && (
-              <div
-                className="absolute right-2 top-0 flex h-6 items-center text-xs font-semibold text-white"
-                style={{ right: "10px" }}
-              >
-                +{curState.lastGuess.points} points
+            {curState.points > 300 && (
+              <div className="relative flex items-center justify-center text-center text-xs text-white">
+                +<br />
+                {curState.lastGuess.points}
               </div>
             )}
-            {(curState.points >= 500) && (
-              <div
-                className="absolute right-2 top-0 flex h-6 items-center text-xs font-semibold text-white"
-                style={{ right: "10px" }}
-              >
-                +{curState.lastGuess.points} points ({curState.lastGuess.distance}m away)
-              </div>
-            )} */}
-            {curState.points > 50 && (
-              <div
-                className="absolute right-2 top-0 flex h-6 items-center text-xs font-semibold text-white"
-                style={{ right: "10px" }}
-              >
-                +{curState.lastGuess.points}
-              </div>
-            )}
-          </div>
-
-          {Array.from({ length: 4 }, (_, i) => (
-            <div
-              key={i}
-              className="absolute top-0 z-[1250] h-6 border-r border-slate-200 opacity-50"
-              style={{ left: `${(i + 1) * 20}%` }}
-            >
-              {/* <span className="text-white absolute ml-1">{`${(i + 1) * 1000}`}</span> */}
-            </div>
-          ))}
-
-          {/* Text below the triangle */}
-          <div
-            className="absolute z-[1260] px-4 text-center font-semibold text-red-700"
-            style={{
-              left: `${progress}%`,
-              bottom: "-2.5vh",
-              backgroundColor: "rgba(255, 255, 255, 0.7)",
-              borderRadius: "10px",
-              backdropFilter: "blur(10px)",
-              boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-            }}
-          >
-            ^ You have {curState.points} points!
           </div>
         </div>
       </div>
+
       <div className="pointer-events-auto absolute bottom-6 left-0 right-0 z-[1300] flex justify-center">
         <MotionButton
           onClick={() => {
