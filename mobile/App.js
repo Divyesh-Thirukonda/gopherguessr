467ddab3e78079ade0ccc1a4b3d0b5822bd11cc3
jin hammond <121529261+jinexists@users.noreply.github.com>
2025-02-18 14:03:26 -0600

    add webview

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from "react-native-webview";
+import Constants from "expo-constants";
+import { StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      style={styles.container}
+      source={{ uri: "https://gopher.nimbus.page" }}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
   },
 });
