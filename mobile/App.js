3abb2e00fc3b963dae96c683c694db633fde02f7
jin hammond <121529261+jinexists@users.noreply.github.com>
2025-02-18 18:45:36 -0600

    attempt google login

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,7 @@
 import { WebView } from "react-native-webview";
 import Constants from "expo-constants";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Linking } from "react-native";
+import * as WebBrowser from "expo-web-browser";
 
 export default function App() {
   return (
@@ -8,6 +9,14 @@ export default function App() {
       style={styles.container}
       source={{ uri: "https://gopher.nimbus.page" }}
       allowsInlineMediaPlayback={true}
+      webviewDebuggingEnabled={true}
+      javaScriptCanOpenWindowsAutomatically={true}
+      onOpenWindow={(syntheticEvent) => {
+        const { nativeEvent } = syntheticEvent;
+        const { targetUrl } = nativeEvent;
+        WebBrowser.openBrowserAsync(targetUrl);
+      }}
+      userAgent="Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1"
     />
   );
 }
