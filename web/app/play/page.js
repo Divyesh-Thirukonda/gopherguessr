3528dca4d66d48c11d5a025a82d54507ce11b71a
jin hammond <121529261+jinexists@users.noreply.github.com>
2025-02-25 16:04:29 -0600

    thought we fixed this one

--- web/app/play/page.js
+++ web/app/play/page.js
@@ -427,6 +427,7 @@ export default async function Play({ searchParams }) {
         isLoggedIn={userInDB}
         isTimed={isTimed.toLowerCase() === "true"}
         gameMode={gameMode}
+        goHome={goHome}
       />
       {curLobby && !curState.lobbyUsername && (
         <div className="fixed inset-0 z-[3000] flex items-center justify-center bg-white bg-opacity-50 backdrop-blur-md">
