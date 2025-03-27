bb9714143301204f469ff879129920f3603c6115
Samuel Breider <samueljbreider@gmail.com>
2025-03-27 13:16:06 -0500

    fix messaging

--- web/app/lobby/_components/LobbyLeaderboard.js
+++ web/app/lobby/_components/LobbyLeaderboard.js
@@ -45,7 +45,7 @@ export default function LobbyLeaderboard({ games, path, pollForLeaderboard }) {
           {
             <div>
               <h1 className="text-3xl font-bold text-white">
-                Let&apos;s see how you did...
+                The current standings...
               </h1>
               <div className="mt-3 flex flex-col items-center justify-center overflow-hidden rounded-xl border bg-white p-4">
                 <h2 className="text-2xl font-medium">Leaderboard</h2>
